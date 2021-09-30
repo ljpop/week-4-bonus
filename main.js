c59db7edc3aa26625737c23609eaf2a33bb45f8b
ljpop <80583587+ljpop@users.noreply.github.com>
2021-09-30 16:21:50 +0200

    JS done using functions

--- main.js
+++ main.js
@@ -1,114 +1,50 @@
 "use strict";
 
-/*TODO: one sentance, change word*/
-
-
 const errorFirstName = "First Name cannot be empty"; 
 const errorLastName = "Last Name cannot be empty"; 
 const errorEmail = "Email cannot be empty"; 
 const errorPass = "Password cannot be empty"; 
-
 const errorWrong = "Looks like this is not an email"; 
 
-const displayMessageFName = function (message) {
-  document.querySelector(".errorMessage1").textContent = message;
-  document.querySelector(".errorMessage1").classList.add("visible");
-  document.querySelector('[name="fname"]').classList.add("errorClass");
-  document.querySelector('[name="fname"]').classList.add("error-icon");  
-  document.querySelector('[name="fname"]').placeholder='';  
+const displayMessage = function (message, id, errorClass) {
+  document.querySelector(errorClass).textContent = message;
+  document.querySelector(errorClass).classList.add("visible");
+  document.getElementById(id).classList.add("errorClass");
+  document.getElementById(id).classList.add("error-icon");  
+  document.getElementById(id).placeholder='';  
 };
 
-const removeFName = function (message) {
-  document.querySelector(".errorMessage1").textContent = message;
-  document.querySelector(".errorMessage1").classList.remove("visible");
-  document.querySelector('[name="fname"]').classList.remove("errorClass");
-  document.querySelector('[name="fname"]').classList.remove("error-icon"); 
+const removeError = function (id, error) {
+  document.querySelector(error).textContent = '';
+  document.querySelector(error).classList.remove("visible");
+  document.getElementById(id).classList.remove("error-icon");    
+  document.getElementById(id).classList.remove("errorClass");
 };
-
-const displayMessageLName = function (message) {
-  document.querySelector(".errorMessage2").textContent = message;
-  document.querySelector(".errorMessage2").classList.add("visible");
-  document.querySelector('[name="lname"]').classList.add("errorClass");
-  document.querySelector('[name="lname"]').classList.add("error-icon");  
-  document.querySelector('[name="lname"]').placeholder=''; 
-};
-
-const removeLName = function (message) {
-  document.querySelector(".errorMessage2").textContent = message;
-  document.querySelector(".errorMessage2").classList.remove("visible");
-  document.querySelector('[name="lname"]').classList.remove("errorClass");
-  document.querySelector('[name="lname"]').classList.remove("error-icon"); 
-};
-
-const displayMessageMail = function (message) {
-  document.querySelector(".errorMessage3").textContent = message;
-  document.querySelector(".errorMessage3").classList.add("visible");
-  document.querySelector('[name="mail"]').classList.add("errorClass");
-  document.querySelector('[name="mail"]').classList.add("error-icon");  
-  document.querySelector('[name="mail"]').placeholder='';  
-}
-
-const removeMail = function (message) {
-  document.querySelector(".errorMessage3").textContent = message;
-  document.querySelector(".errorMessage3").classList.remove("visible");
-  document.querySelector('[name="mail"]').classList.remove("errorClass");
-  document.querySelector('[name="mail"]').classList.remove("error-icon"); 
-};
-
-const displayMessagePass = function (message) {
-  document.querySelector(".errorMessage4").textContent = message;
-  document.querySelector(".errorMessage4").classList.add("visible");
-  document.querySelector('[name="password"]').classList.add("errorClass");
-  document.querySelector('[name="password"]').classList.add("error-icon");  
-  document.querySelector('[name="password"]').placeholder=''; 
-};
-
-const removePass = function (message) {
-  document.querySelector(".errorMessage4").textContent = message;
-  document.querySelector(".errorMessage4").classList.remove("visible");
-  document.querySelector('[name="password"]').classList.remove("errorClass");
-  document.querySelector('[name="password"]').classList.remove("error-icon"); 
-};
-
 
 const validateEmail = function (email) {
   const re = /\S+@\S+\.\S+/;
   return re.test(email);
 };
 
-const everythingOk = function () {
-  // removeMessage(); TODO:
-alert('Everything is OK :)')
-};
+const checkEmpty=function(errorMsg, id, errorClass){
+  const inputVal = document.getElementById(id).value;
+  if (inputVal == "" || inputVal == " ") {displayMessage(errorMsg, id, errorClass); return true;}
+  else {removeError(id, errorClass); return false;}  
+}
 
-const removeMessage = function () {
-  document.querySelectorAll(".errorMessage").classList.remove("visible");
-  document.querySelectorAll(".input-form").classList.remove("errorClass");  
-  document.querySelectorAll(".input-form").classList.remove("error-icon");
-};
+const checkWrong=function(errorMsg, id, errorClass){ 
+  const inputVal = document.getElementById(id).value;
+  if (!validateEmail(inputVal)) displayMessage(errorMsg, id, errorClass);
+  else removeError(id, errorClass);
+}
 
 const doCheck = function (event) {  
   event.preventDefault();
-
-  const fname = document.querySelector('[name="fname"]').value;
-  const lname = document.querySelector('[name="lname"]').value;
-  const mail = document.querySelector('[name="mail"]').value;
-  const pass = document.querySelector('[name="password"]').value;
- 
-  if (fname == "" || fname == " ") displayMessageFName(errorFirstName);
-  else removeFName();
-
-  if (lname == "" || lname == " ") displayMessageLName(errorLastName); 
-  else removeLName(); 
-
-  if (pass == "" || pass == " ") displayMessagePass(errorPass);
-  else removePass();
-
-  if (mail == "" || mail == " ") displayMessageMail(errorEmail);  
-  else if (!validateEmail(mail)) displayMessageMail(errorWrong);
-  else removeMail();  
-
-
-  
+  checkEmpty(errorFirstName, 'fname', ".errorMessage1");
+  checkEmpty(errorLastName, 'lname', ".errorMessage2");    
+  if(checkEmpty(errorEmail, 'mail', ".errorMessage3")) 
+    {checkEmpty(errorEmail, 'mail', ".errorMessage3");} 
+  else checkWrong(errorWrong, 'mail', ".errorMessage3");   
+  checkEmpty(errorPass, 'password', ".errorMessage4");  
 };
 
